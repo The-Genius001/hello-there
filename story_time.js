d09008a2fed32f22b2fd2edea6b9066b29f84705
The-Genius001 <83796789+The-Genius001@users.noreply.github.com>
2021-12-11 15:02:09 -0600

    gutan tag

---
+++ story_time.js
@@ -0,0 +1,33 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let hello ="hello";
+let hi = "my name is";
+let gutanTag = "theGenius";
+let hallo = "and I am going to tell you a story";
+
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let time = "1990"
+let place = "MIT"
+let otherthing = "this other place"
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let Items = ["laptop", "mouse", "backpack"]
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let learning = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(hello);
+console.log(hi);
+console.log(gutanTag);
+console.log(hallo);
+console.log("it is the year" + time);
+console.log("And a guy is working at" + place);
+console.log("He lives at" + otherthing)
+console.log("he has these things in his backpack:"+ Items[2]);
+console.log("bye");
